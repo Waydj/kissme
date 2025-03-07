034719b74ef4bec21327a9bab3a6536778a8f023
Waydj <farhminait@gmail.com>
2025-03-07 13:30:02 +0200

    base folder structure, types, consts

---
+++ src/data/index.ts
@@ -0,0 +1,54 @@
+import { IUser } from "../types";
+
+export const USERS: IUser[] = [
+  {
+    id: 1,
+    avatar: "/profile/Regular face 1.png",
+    name: "John",
+  },
+  {
+    id: 2,
+    avatar: "/profile/Regular face 2.png",
+    name: "Alice",
+  },
+  {
+    id: 3,
+    avatar: "/profile/Regular face 3.png",
+    name: "Mary",
+  },
+  {
+    id: 4,
+    avatar: "/profile/Regular face 4.png",
+    name: "Alex",
+  },
+  {
+    id: 5,
+    avatar: "/profile/Regular face 5.png",
+    name: "Ivan",
+  },
+  {
+    id: 6,
+    avatar: "/profile/Regular face 6.png",
+    name: "Nikolay",
+  },
+  {
+    id: 7,
+    avatar: "/profile/Regular face 7.png",
+    name: "Anna",
+  },
+  {
+    id: 8,
+    avatar: "/profile/Regular face 8.png",
+    name: "Elena",
+  },
+  {
+    id: 9,
+    avatar: "/profile/Regular face 9.png",
+    name: "John Doe",
+  },
+  {
+    id: 10,
+    avatar: "/profile/Regular face 10.png",
+    name: "Tom",
+  },
+];
